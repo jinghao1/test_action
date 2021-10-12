54a7b6ef183090bd3311fd0f163cb44b49d728b3
lostsnow <lostsnow@gmail.com>
2021-10-12 11:11:39 +0800

    fixes flask

--- testFlask/app.py
+++ testFlask/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from dongtai_agent_python.middlewares.flask_middleware import AgentMiddleware
 
+
 app = Flask(__name__)
+app.wsgi_app = AgentMiddleware(app.wsgi_app, app)
 
 
 @app.route('/')
@@ -10,5 +12,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.wsgi_app = AgentMiddleware(app.wsgi_app, app)
     app.run()
